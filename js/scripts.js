51519bbf139660050b223ab4c4ca1bb15257854c
Zephan The <zephan.the@berkeley.edu>
2019-10-20 01:45:06 -0700

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,7 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'c2b4b86c677b969fdf2b9701b868e96f') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbwrsTG7YumsEio-AMA1VC1E6Aml-1InEjQZvjHaIT_ODFjOoFg/exec', data)
